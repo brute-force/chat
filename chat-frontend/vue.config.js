045bff9a803949e4a1bec0dd017a8ed6a94ba5bb
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-19 14:17:11 -0400

    remove commented out code

--- chat-frontend/vue.config.js
+++ chat-frontend/vue.config.js
@@ -1,6 +1,3 @@
-// const VueLoaderPlugin = require('');
-// const VueLoaderPlugin = require('vue-loader/lib/plugin');
-
 module.exports = {
   lintOnSave: false,
   devServer: {
@@ -8,29 +5,7 @@ module.exports = {
       '/chatty/*': {
         target: 'ws://localhost:3000',
         ws: true
-        // logLevel: 'debug'
       }
     }
   }
-  // ,
-  // configureWebpack: {
-  //   plugins: [
-  //     new VueLoaderPlugin()
-  //   ]
-  // },
-  // chainWebpack: (config) => {
-  //   config.module
-  //     .rule('vue')
-  //     .test(/\.vue$/)
-  //     .use('vue-loader')
-  //     .loader('vue-loader')
-  //     .end();
-
-  //   config.module
-  //     .rule('css')
-  //     .test(/\.s?css$/)
-  //     .use('vue-loader')
-  //     .loader('vue-loader')
-  //     .end();
-  // }
 };

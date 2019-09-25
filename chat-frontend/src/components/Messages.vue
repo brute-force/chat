41b4cd0f325c369fe65037cd4352aebc552d5b48
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-25 00:44:20 -0400

    new messages component -move messages div to separate component -correctly update scrolling behavior on new messages

---
+++ chat-frontend/src/components/Messages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="messages" class="chat__messages" ref="messages">
+    <div class="message" v-for="message in messages" :key="message.id">
+      <p>
+        <span class="message__name" v-html="message.username"></span>
+        <span class="message__meta">{{ message.createdAt }}</span>
+      </p>
+      <p v-html="message.message"></p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Messages',
+  props: {
+    messages: Array
+  },
+  updated () {
+    this.$refs.messages.scrollTop = this.$refs.messages.lastElementChild.offsetTop;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/messages.scss";
+</style>

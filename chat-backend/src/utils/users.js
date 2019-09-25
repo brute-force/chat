559c66aeb287036cbbb8f5b916b8029bd66b5ea1
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-25 00:51:26 -0400

    add getUsers() for debugging

--- chat-backend/src/utils/users.js
+++ chat-backend/src/utils/users.js
@@ -46,22 +46,15 @@ const getRooms = () => {
   return users.map((user) => user.room).filter((room, i, rooms) => rooms.indexOf(room) === i);
 };
 
-// const updateUserTypingStatus = (id, room, isTyping) => {
-//   const index = users.findIndex((user) => user.id === id && user.room === room);
-
-//   if (index !== -1) {
-//     const user = users[index];
-//     user.isTyping = isTyping;
-
-//     return users.splice(index, 1, user)[0];
-//   }
-// };
+const getUsers = () => {
+  return users;
+};
 
 module.exports = {
   addUser,
   removeUserFromRoom,
   getUser,
   getUsersInRoom,
-  // updateUserTypingStatus,
-  getRooms
+  getRooms,
+  getUsers
 };
